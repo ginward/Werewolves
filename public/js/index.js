018ec8380bfb0e6f69b7438b0894e62ebec0a041
班章 <localhost>
2017-03-13 12:44:26 -0400

    added button

--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,13 @@ var swiper = new Swiper('.swiper-container', {
     pagination: '.swiper-pagination',
     paginationClickable: true,
 });
+
+$('#wolf-select').multiSelect()
+
+$('#number_of_players').selectivity({
+    allowClear: true,
+    items: ['9', '10', '11', '12'],
+    placeholder: '请选择'
+});
+
+document.addEventListener("touchstart", function() {},false); // add this junk to make iOS observe the :active state for touch
