1b7a154d8a7c012db4d6d812d985b76d3b20d512
班章 <localhost>
2017-03-17 15:41:21 -0400

    addded the role count

--- public/cards/game.js
+++ public/cards/game.js
@@ -7,4 +7,8 @@ jQuery(document).ready(function(){
 
 function nextPage(){
 	swiper.slideNext();
+	if(jQuery(".wolf")!=null){
+		//we are at the wolf page
+		//create the number of options based on the number of players
+	}
 }
--- public/cards/setup.js
+++ public/cards/setup.js
@@ -3,33 +3,47 @@ jQuery(document).ready(function(){
 		data.role_count.god = jQuery("#num_god").val();
 		data.role_count.witch = jQuery("#witch").val();
 		data.role_count.people = jQuery("#num_people").val();
-		nextPage();
+		nextPageSetup();
 	});
 	jQuery('body').on('click','#game_start', function(){
 		launchGame();
 	});
-	$('#num_god').selectivity({
+	jQuery('#num_god').selectivity({
 	    allowClear: true,
 	    items: ['9', '10', '11', '12'],
 	    placeholder: '请选择'
 	});
-	$('#num_wolf').selectivity({
+	jQuery('#num_wolf').selectivity({
 	    allowClear: true,
 	    items: ['9', '10', '11', '12'],
 	    placeholder: '请选择'
 	});
-	$('#num_people').selectivity({
+	jQuery('#num_people').selectivity({
 	    allowClear: true,
 	    items: ['9', '10', '11', '12'],
 	    placeholder: '请选择'
 	});
 });
 
-function nextPage(){
+function nextPageSetup(){
 	swiper.slideNext();
 }
 
+//initialize the roles in database and set the roles to null
+function init_roles(){
+	data.players = []; // re-empty the array
+	var num_god = jQuery("#num_god").val();
+	var num_people = jQuery("#num_people").val();
+	var num_wolf = jQuery("#num_wolf").val();
+	var total_num = int(num_god) + int(num_people) + int(num_wolf);
+	for (var i=0;i<total_num;i++){
+		var player = new Player("");
+		data.players.push(player);
+	}
+}
+
 function launchGame(){
+	var num_god = jQuery("#num_god").val();
 	//prepare the set of cards and launch the game
 	remove_card(".card_setup");
 	if ('cupid' in data.active_roles.god){
